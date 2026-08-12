107f84a13f49cc98498b47cb7f15a4dc869ab9b0
fedos <fedrodj@gmail.com>
2026-08-12 17:04:42 +0300

    :heavy_plus_sign: new lib for ui

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ if [[ -d "$BUILD_DIR" ]]; then
   rm -rf $BUILD_DIR
 fi
 
+# -DCMAKE_BUILD_TYPE=Release \
 cmake -S . -B "$BUILD_DIR" -G Ninja \
-  -DCMAKE_BUILD_TYPE=Release
-  # -DCMAKE_BUILD_TYPE=Debug \
-  # -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cmake --build "$BUILD_DIR" --parallel
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 #include "appstyle.h"
 #include "mainwindow.h"
+#include "oclero/qlementine/style/QlementineStyle.hpp"
 
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
 
-  AppStyle::apply(a);
+  //AppStyle::apply(a);
+  auto style = new oclero::qlementine::QlementineStyle(&a);
+  QApplication::setStyle(style);
 
   MainWindow w;
   w.show();
